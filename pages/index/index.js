1daf504dd8488c4126b1ffbac1b8db16dfac6e13
LAPTOP-84HD0QEF\DXQ <746919174@qq.com>
2022-11-21 15:14:55 +0800

    no message

--- pages/index/index.js
+++ pages/index/index.js
@@ -24,6 +24,7 @@ Page({
       })
     }
   },
+  onShow() {},
   getUserProfile(e) {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
@@ -44,5 +45,9 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  }
+  },
+  // 跳转
+  goDateTimer() {
+    wx.navigateTo({url: '../dateTimer/dateTimer'})
+  },
 })
